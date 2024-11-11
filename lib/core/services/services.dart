0dd2c4a8169b610f39e7af801d22b4639d83cda1
George Al Nasser <148635405+georgeAlNa@users.noreply.github.com>
2024-11-11 13:39:36 +0200

    Update

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -8,17 +8,7 @@ class MyService extends GetxService {
   late SharedPreferences sharedPreferences;
 
   Future<MyService> init() async {
-    // Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-    // await Firebase.initializeApp(
-    // name: 'training-app-12f6c',
-    // options: const FirebaseOptions(
-    //   apiKey: 'AIzaSyCe8xmxodKu2Ed9V0RhLf-PJks-ArYX3pA',
-    //   appId: '1:811733201627:android:64a66c7fba4c62fbe212fe',
-    //   messagingSenderId: '811733201627',
-    //   projectId: 'training-app-12f6c',
-    //   storageBucket: 'training-app-12f6c.appspot.com',
-    // ),
-    // );
+ 
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
     final token = await FirebaseMessaging.instance.getToken();
